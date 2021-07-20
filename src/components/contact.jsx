66a52dd600d1c75b792f2707668ae1e2c9accaa9
Calvin Yuen <yuensc07@gmail.com>
2021-07-20 14:31:04 +0800

    Update Contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,26 +1,33 @@
 import React, { Component } from "react";
 
 class Contact extends Component {
+  state = {
+    personalInfo: [
+      { label: "Name", data: "Yuen Sing Chun, Calvin" },
+      { label: "Phone No.", data: "5168 7161" },
+      { label: "Email", data: "scyuenab@gmail.com" },
+      {
+        label: "Github of this website",
+        data: "https://github.com/YuenSC/myDemo",
+      },
+    ],
+  };
   render() {
     return (
       <div className="container">
-        <div style={{ marginTop: 30 }} className="row">
-          <div className="col-3">
-            <h1>Name:</h1>
-          </div>
-          <div className="col-3">
-            <h2>Yuen Sing Chun, Calvin</h2>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col-3">
-            <h1>Contact Numbers:</h1>
-          </div>
-          <div className="col-3">
-            <h2>5168 7161</h2>
-          </div>
-        </div>
+        {this.state.personalInfo.map(({ label, data }) => (
+          <React.Fragment key={label}>
+            <div style={{ marginTop: 30 }} className="row">
+              <div className="col-3">
+                <h1>{`${label} : `}</h1>
+              </div>
+              <div className="col-3">
+                <h2>{data}</h2>
+              </div>
+            </div>
+            <hr />
+          </React.Fragment>
+        ))}
       </div>
     );
   }
