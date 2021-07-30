7713b97310f4f6d209db27c9319cdcaa53ccc631
Calvin Yuen <scyuenab@gmail.com>
2021-07-31 00:10:51 +0800

    add a server for unsplash api

---
+++ server/config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ACCESS_KEY:
+    process.env.ACCESS_KEY || "DGcFPoThTbCHYAFsecioLBIBL_FEID6GoNiD0NpIcRI",
+  SECRET: process.env.SECRET || "bBX-NSHvZjZJXQh2W_l9LqUtgH-qvu2rfPd4h2505Rk",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000",
+};
