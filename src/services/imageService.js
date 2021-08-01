50a5709c79cb74cd1a7c7eb66887a3ab756ab75f
Calvin Yuen <scyuenab@gmail.com>
2021-08-01 12:29:48 +0800

    budfix in image Services

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -4,6 +4,6 @@ const apiEndPoint = "/photos";
 
 export function getPhotos(query, page, perPage) {
   return HttpService.get(
-    `/api/${apiEndPoint}?query=${query}&page=${page}&perPage=${perPage}`
+    `api/${apiEndPoint}?query=${query}&page=${page}&perPage=${perPage}`
   );
 }
